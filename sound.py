3420a7f11326c65a24446686dc07d385d46cc098
jamesbiddle <james.biddle@adelaide.edu.au>
2021-11-04 16:24:43 +1030

    Implemented saving animations as gifs.
    Uses imagemagick to save and gifsicle to compress.

--- sound.py
+++ sound.py
@@ -1,11 +1,10 @@
 import numpy as np
 from numpy import sin, cos, pi
+from shutil import which
 from numpy.random import rand
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from matplotlib.widgets import Slider
-# from ipywidgets import interact, interactive, fixed, interact_manual
-from math import ceil
+import subprocess
 import os
 from math import gcd
 
@@ -19,6 +18,16 @@ def find_lcm(array):
 
 class Particle:
     def __init__(self, x0, y0, p, k, w):
+        """Initialise the particle
+
+        :param x0: Initial x position
+        :param y0: Initial y position
+        :param p: Pressure
+        :param k: Wavenumber
+        :param w: angular frequency
+
+        """
+
         self.x0 = x0
         self.y0 = y0
         self.p = p
@@ -29,6 +38,11 @@ class Particle:
         self.y = y0
 
     def set_pos(self, t):
+        """Set the x position of the particle at time t
+
+        :param t: Time to evaluate the position
+
+        """
         self.x = self.x0 + (self.p / self.k) * cos(self.k * self.x0 - self.w * t)
 
     def __str__(self):
@@ -40,7 +54,6 @@ class Animator:
         self.simulations = {}
         self.scatters = {}
         self.plots = {}
-        self.widgets = {}
 
         self.key = 1
 
@@ -48,45 +61,72 @@ class Animator:
         self.simulations[self.key] = Simulation(*args, **kwargs)
         self.key += 1
 
-    def remove_simulation(self, key):
-        del self.simulations[key]
+    def reset(self):
+        self.__init__()
+
+    def save(self, filename, fps=30, dpi=None, compress=True):
+        """Save the simulation as a gif
+
+        :param filename: Output name
+        :param fps: Framerate of the gif
+        :param dpi: (optional, default=None) Gif dpi. If None, use imagemagick default
+        :param compress: (Optional, default=True) Compress with gifsicle
+
+        """
+        if not which('convert'):
+            print('Gif writer not found, cannot output animation.')
+            return
+        anim = self.run()
+        if dpi is None:
+            anim.save(filename,
+                      writer='imagemagick', fps=fps)
+        else:
+            anim.save(filename,
+                      writer='imagemagick', fps=fps, dpi=dpi)
+
+        if compress:
+            bashcmd = f'gifsicle -i {filename} -O3 --colors 128 -o {filename}'
+            subprocess.run(bashcmd, shell=True)
+
+        print(f'Saved {filename}')
 
     def run(self):
-        self.setup_plot()
+        self._setup_plot()
         frames = self._get_interval()
         self.animation = animation.FuncAnimation(self.fig,
-                                                 self.update,
-                                                 interval=20,
-                                                 repeat=True,
-                                                 repeat_delay=5,
+                                                 self._update,
+                                                 interval=40,
+                                                 repeat=False,
                                                  frames=frames,
                                                  blit=True)
 
         return self.animation
 
-    def _get_interval(self):
+    def _get_interval(self, max_frames=200):
         periods = [1 / self.simulations[key].f
                    for key in self.simulations]
-        print(periods)
         intervals = [self.simulations[key].interval
                      for key in self.simulations]
         frames = [round(T / i) for (T, i) in zip(periods, intervals)]
-        print(frames)
         frames = find_lcm(frames)
-        print(frames)
+        frames = min(max_frames, frames)
         return frames
 
-    def setup_plot(self):
-        n_sim = len(self.simulations)
-        widths = [1, 15]
-        heights = n_sim * [4, 1]
+    def _setup_plot(self):
+        """Initialise the plot once all simulations have been added
 
-        self.fig = plt.figure(constrained_layout=True)
-        gs = self.fig.add_gridspec(2 * n_sim, 2,
-                                   width_ratios=widths,
+        """
+        n_sim = len(self.simulations)
+        heights = n_sim * [4, 1]
+        default_height = 4.8
+        default_width = 6.4
+        size = [default_width, (n_sim / 2) * default_height]
+
+        self.fig = plt.figure(constrained_layout=True, figsize=size)
+        gs = self.fig.add_gridspec(2 * n_sim, 1,
                                    height_ratios=heights)
         for i, key in enumerate(self.simulations):
-            ax_sim, ax_func, ax_slider = self.add_sim_grid(gs, i)
+            ax_sim, ax_func = self._add_sim_grid(gs, i)
 
             # Setup particle sim
             sim = self.simulations[key]
@@ -107,37 +147,19 @@ class Animator:
             ax_func.set_ylabel('Pressure')
             ax_func.set_xlabel('Position')
 
-            # Set slider bars
-            self.widgets[key] = self.add_slider(ax_slider, key)
+    def _add_sim_grid(self, gs, i):
+        """Add a simulation to the animation
 
-    def add_slider(self, ax, key):
-        sim = self.simulations[key]
-        p_slider = Slider(
-                ax=ax,
-                label='Pressure',
-                valmin=0,
-                valmax=1,
-                valinit=sim.p0,
-                orientation='vertical'
-            )
+        :param gs: Gridspec to use
+        :param i: Which simulation is being added
+        :returns: list of axes
 
-        def update(val):
-            slider = self.widgets[key]
-            sim.p0 = slider.val
-            for p in sim.particles:
-                p.p = slider.val
-            self.fig.canvas.draw_idle()
+        """
+        ax1 = self.fig.add_subplot(gs[2 * i, 0])
+        ax2 = self.fig.add_subplot(gs[2 * i + 1, 0])
+        return [ax1, ax2]
 
-        p_slider.on_changed(update)
-        return p_slider
-
-    def add_sim_grid(self, gs, i):
-        ax1 = self.fig.add_subplot(gs[2 * i, 1])
-        ax2 = self.fig.add_subplot(gs[2 * i + 1, 1])
-        ax3 = self.fig.add_subplot(gs[2 * i:2 * i + 1, 0])
-        return [ax1, ax2, ax3]
-
-    def update(self, j):
+    def _update(self, j):
         for key in self.simulations:
             sim = self.simulations[key]
             data = sim.update()
@@ -175,10 +197,10 @@ class Simulation:
         self.w = 2 * pi * f
         self.t = 0
         self.interval = 0.01
-        self.x_range = np.linspace(0, self.width)
+        self.x_range = np.linspace(0, self.width, num=100)
 
         for i in range(self.n - 1):
-            x, y = self.random_position()
+            x, y = self._random_position()
             self.particles.append(Particle(x, y, self.p0, self.k, self.w))
 
         # Always put 1 particle in the middle to highlight
@@ -206,7 +228,7 @@ class Simulation:
         data = np.array([x, y]).T
         return data
 
-    def random_position(self):
+    def _random_position(self):
         y = rand() * self.height
         x = rand() * (self.width + 2 * self.p0) - self.p0
         return x, y
@@ -216,17 +238,33 @@ class Simulation:
 
 
 if __name__ == '__main__':
-    sim = Animator()
-    sim.add_simulation(f=3)
-    sim.add_simulation(f=2)
-    anim = sim.run()
-    # plt.show()
-
     out_dir = './output/'
-    filename = os.path.join(out_dir, 'animation.html')
-    html = anim.to_html5_video()
-    with open(filename, 'w') as f:
-        f.write(html)
-    plt.close('all')
-    # anim.save(filename,
-    #           writer='imagemagick', fps=30)
+
+    sim = Animator()
+    sim.add_simulation(p0=0.2)
+    sim.add_simulation(p0=0.5)
+    sim.add_simulation(p0=1)
+    filename = os.path.join(out_dir, 'sound_p0_comp.gif')
+    sim.save(filename, dpi=100)
+    sim.reset()
+
+    sim.add_simulation(l=0.2, p0=0.8)
+    sim.add_simulation(l=1, p0=0.8)
+    sim.add_simulation(l=2, p0=0.8)
+    filename = os.path.join(out_dir, 'sound_l_comp.gif')
+    sim.save(filename, dpi=100)
+    sim.reset()
+
+    sim.add_simulation(f=0.5, p0=0.8)
+    sim.add_simulation(f=1, p0=0.8)
+    sim.add_simulation(f=2, p0=0.8)
+    filename = os.path.join(out_dir, 'sound_f_comp.gif')
+    sim.save(filename, dpi=100)
+    sim.reset()
+
+    sim.add_simulation(f=0.5, p0=0.8, l=2)
+    sim.add_simulation(f=1, p0=0.8, l=1)
+    sim.add_simulation(f=2, p0=0.8, l=0.5)
+    filename = os.path.join(out_dir, 'sound_samev.gif')
+    sim.save(filename, dpi=100)
+    sim.reset()
